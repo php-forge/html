b60d0a47775a73aed3ddcd2125255066069ade7e
Wilmer Arambula <terabytesoftw@gmail.com>
2022-05-31 13:11:26 -0400

    Corregir phpdocs, refactorizar etiqueta nav.

--- src/Tag/Element/A.php
+++ src/Tag/Element/A.php
@@ -7,17 +7,19 @@ namespace Forge\Html\Tag\Element;
 use Forge\Html\Attribute\Attributes;
 use Forge\Html\Tag\Tag;
 
+/**
+ * The <a> HTML element (or anchor element), with its href attribute, creates a hyperlink to web pages, files, email
+ * addresses, locations in the same page, or anything else a URL can address.
+ *
+ * @link https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-a-element
+ */
 final class A
 {
     /**
-     * The <a> HTML element (or anchor element), with its href attribute, creates a hyperlink to web pages, files, email
-     * addresses, locations in the same page, or anything else a URL can address.
-     *
-     * @link https://html.spec.whatwg.org/multipage/text-level-semantics.html#the-a-element
+     * The created <a> HTML element.
      *
      * @param array $attributes The tag attributes in terms of name-value pairs. These will be rendered as the
      * attributes of the resulting tag. The values will be HTML-encoded using {@see Attributes::encode()}.
-     *
      * See {@see Attributes::render()} for details on how attributes are being rendered.
      * @param string $content The content of the tag.
      *
