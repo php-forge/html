16af9b8d963ac3b07cd70afe37bdaf5119a8eab3
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-05-31 14:01:04 -0400

    Simplificar pruebas unitarias etiqueta nav. (#9)
    * Simplificar pruebas unitarias etiqueta nav.

--- tests/Tag/Element/NavTest.php
+++ tests/Tag/Element/NavTest.php
@@ -4,35 +4,34 @@ declare(strict_types=1);
 
 namespace Forge\Html\Tests\Tag\Element;
 
-use Forge\Html\Tag\Element\A;
 use Forge\Html\Tag\Element\Nav;
-use Forge\TestUtils\Assert;
 use PHPUnit\Framework\TestCase;
 
 final class NavTest extends TestCase
 {
-    public function createProvider(): array
+    public function beginProvider(): array
     {
         return [
-            [[], '', '<nav>' . PHP_EOL . '</nav>'],
-            [['class' => 'class'], '', '<nav class="class">' . PHP_EOL . '</nav>'],
-            [[], 'Content' . PHP_EOL, '<nav>' . PHP_EOL . 'Content' . PHP_EOL . '</nav>'],
-            [['disabled' => true], '', '<nav disabled>' . PHP_EOL . '</nav>'],
+            [[], '<nav>'],
+            [['class' => 'class', 'disabled' => true], '<nav class="class" disabled>'],
         ];
     }
 
     /**
-     * @dataProvider createProvider
+     * @dataProvider beginProvider
      *
      * @param array $attributes Tag attributes.
-     * @param string $content Tag content.
      * @param string $expected Expected result.
      */
-    public function testCreate(array $attributes, string $content, string $expected): void
+    public function testBegin(array $attributes, string $expected): void
     {
-        $a = new A();
-        $assert = new Assert();
         $nav = new Nav();
-        $assert->equalsWithoutLE($expected, $nav->begin($attributes) . $content . $nav->end());
+        $this->assertSame($expected, $nav->begin($attributes));
+    }
+
+    public function testEnd(): void
+    {
+        $nav = new Nav();
+        $this->assertSame('</nav>', $nav->end());
     }
 }
