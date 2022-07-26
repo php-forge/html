ac0c20802d4f7e4eba23c9a3b8b282d4c3860648
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-07-26 16:44:35 -0400

    Agregar metodo `normalizeRegexpPattern()`. (#33)
    * Agregar método normalizeRegexpPattern().

--- tests/Helper/UtilsTest.php
+++ tests/Helper/UtilsTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Forge\Html\Tests\Helper;
 
 use Forge\Html\Helper\Utils;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 final class UtilsTest extends TestCase
@@ -20,5 +21,56 @@ final class UtilsTest extends TestCase
         $this->assertSame('登录[]', Utils::generateArrayableName('登录[]'));
         $this->assertSame('登录[0][]', Utils::generateArrayableName('登录[0]'));
         $this->assertSame('[0]登录[0][]', Utils::generateArrayableName('[0]登录[0]'));
+    }
+
+    public function normalizeRegexpPatternProvider(): array
+    {
+        return [
+            ['', '//'],
+            ['.*', '/.*/'],
+            ['([a-z0-9-]+)', '/([a-z0-9-]+)/Ugimex'],
+            ['([a-z0-9-]+)', '~([a-z0-9-]+)~Ugimex'],
+            ['([a-z0-9-]+)', '~([a-z0-9-]+)~Ugimex', '~'],
+            ['\u1F596([a-z])', '/\x{1F596}([a-z])/i'],
+        ];
+    }
+
+    /**
+     * @dataProvider normalizeRegexpPatternProvider
+     *
+     * @param string $expected The expected result.
+     * @param string $regexp The regexp pattern to normalize.
+     * @param string|null $delimiter The delimiter to use.
+     */
+    public function testNormalizeRegexpPattern(string $expected, string $regexp, ?string $delimiter = null): void
+    {
+        $this->assertSame($expected, Utils::normalizeRegexpPattern($regexp, $delimiter));
+    }
+
+    public function normalizeRegexpPatternInvalidProvider(): array
+    {
+        return [
+            ['', 'Incorrect regular expression.'],
+            ['.*', 'Incorrect regular expression.'],
+            ['/.*', 'Incorrect regular expression.'],
+            ['([a-z0-9-]+)', 'Incorrect regular expression.'],
+            ['/.*/i', 'Incorrect regular expression.', '~'],
+            ['/.*/i', 'Incorrect delimiter.', '//'],
+            ['/~~/i', 'Incorrect delimiter.', '~~'],
+        ];
+    }
+
+    /**
+     * @dataProvider normalizeRegexpPatternInvalidProvider
+     *
+     * @param string $regexp The regexp pattern to normalize.
+     * @param string $message The expected exception message.
+     * @param string|null $delimiter The delimiter to use.
+     */
+    public function testNormalizeRegexpPatternInvalid(string $regexp, string $message, ?string $delimiter = null): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage($message);
+        Utils::normalizeRegexpPattern($regexp, $delimiter);
     }
 }
