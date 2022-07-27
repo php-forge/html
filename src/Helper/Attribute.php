1ce5561838625460c6d1928a3ade4f57ba4109fb
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-07-27 11:53:03 -0400

    Apply fixes from StyleCI (#35)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Helper/Attribute.php
+++ src/Helper/Attribute.php
@@ -4,12 +4,7 @@ declare(strict_types=1);
 
 namespace Forge\Html\Helper;
 
-use function array_unique;
-use function implode;
-use function in_array;
 use function is_array;
-use function is_int;
-use function preg_split;
 
 final class Attribute
 {
