5f79ec1a5e7b99e6dd8e5a87094e9da88265b116
Wilmer Arambula <terabytesoftw@gmail.com>
2022-08-05 18:25:05 -0400

    Corregir style.ci.

--- tests/Attribute/MutationTest.php
+++ tests/Attribute/MutationTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Forge\Form\Tests\Base;
 
 use Forge\Html\Attribute;
-use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 final class MutationTest extends TestCase
