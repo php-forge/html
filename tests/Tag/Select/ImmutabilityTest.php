6fd2cc3ce138dd6471b46d45e77fbfa4e42d7c7e
Wilmer Arambula <terabytesoftw@gmail.com>
2022-08-29 11:21:43 -0400

    Agregar `Select::class`.

---
+++ tests/Tag/Select/ImmutabilityTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Forge\Html\Widgets\Tests\Tag\Select;
+
+use Forge\Html\Tag\Select;
+use PHPUnit\Framework\TestCase;
+use ReflectionException;
+
+final class ImmutabilityTest extends TestCase
+{
+    /**
+     * @throws ReflectionException
+     */
+    public function testImmutability(): void
+    {
+        $select = Select::create();
+        $this->assertNotSame($select, $select->groups([]));
+        $this->assertNotSame($select, $select->items([]));
+        $this->assertNotSame($select, $select->itemsAttributes([]));
+        $this->assertNotSame($select, $select->prompt(''));
+    }
+}
