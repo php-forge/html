4008cf9202e2ea52764287773935fe9c6c8ba533
Wilmer Arambula <terabytesoftw@gmail.com>
2023-07-20 16:49:43 -0400

    Encode `prefix()` and `suffix()` method.

--- src/Attribute/Custom/HasPrefixAndSuffix.php
+++ src/Attribute/Custom/HasPrefixAndSuffix.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PHPForge\Html\Attribute\Custom;
 
+use PHPForge\Html\Helper\Encode;
 use Stringable;
 
 /**
@@ -18,9 +19,14 @@ trait HasPrefixAndSuffix
      * Return new instance specifying the `HTML` prefix content.
      *
      * @param string|Stringable $value The `HTML` prefix content.
+     * @param bool $encode Whether to encode the value.
      */
-    public function prefix(string|Stringable $value): static
+    public function prefix(string|Stringable $value, bool $encode = true): static
     {
+        if (!$value instanceof Stringable && $encode) {
+            $value = Encode::content($value);
+        }
+
         $new = clone $this;
         $new->prefix = (string) $value;
 
@@ -31,9 +37,14 @@ trait HasPrefixAndSuffix
      * Return new instance specifying the `HTML` suffix content.
      *
      * @param string|Stringable $value The `HTML` suffix content.
+     * @param bool $encode Whether to encode the value.
      */
-    public function suffix(string|Stringable $value): static
+    public function suffix(string|Stringable $value, bool $encode = true): static
     {
+        if (!$value instanceof Stringable && $encode) {
+            $value = Encode::content($value);
+        }
+
         $new = clone $this;
         $new->suffix = (string) $value;
 
--- tests/Attribute/Custom/HasPrefixAndSuffixTest.php
+++ tests/Attribute/Custom/HasPrefixAndSuffixTest.php
@@ -20,6 +20,24 @@ final class HasPrefixAndSuffixTest extends TestCase
         $this->assertNotSame($instance, $instance->suffix(''));
     }
 
+    public function testPrefix(): void
+    {
+        $instance = new class () {
+            use HasPrefixAndSuffix;
+
+            protected array $attributes = [];
+
+            public function getPrefix(): string
+            {
+                return $this->prefix;
+            }
+        };
+
+        $this->assertEmpty($instance->getPrefix());
+        $this->assertSame('foo', $instance->prefix('foo')->getPrefix());
+        $this->assertSame('&lt;foo &amp;&amp; bar&gt;', $instance->prefix('<foo && bar>')->getPrefix());
+    }
+
     public function testPrefixStringable(): void
     {
         $instance = new class () {
@@ -44,6 +62,48 @@ final class HasPrefixAndSuffixTest extends TestCase
         $this->assertSame('foo', $instance->prefix($prefix)->getPrefix());
     }
 
+    public function testPrefixStringableSpecialChar(): void
+    {
+        $instance = new class () {
+            use HasPrefixAndSuffix;
+
+            protected array $attributes = [];
+
+            public function getPrefix(): string
+            {
+                return $this->prefix;
+            }
+        };
+
+        $prefix = new class () implements Stringable {
+            public function __toString(): string
+            {
+                return 'foo && bar';
+            }
+        };
+
+        $this->assertEmpty($instance->getPrefix());
+        $this->assertSame('foo && bar', $instance->prefix($prefix)->getPrefix());
+    }
+
+    public function testSuffix(): void
+    {
+        $instance = new class () {
+            use HasPrefixAndSuffix;
+
+            protected array $attributes = [];
+
+            public function getSuffix(): string
+            {
+                return $this->suffix;
+            }
+        };
+
+        $this->assertEmpty($instance->getSuffix());
+        $this->assertSame('foo', $instance->suffix('foo')->getSuffix());
+        $this->assertSame('&lt;foo &amp;&amp; bar&gt;', $instance->suffix('<foo && bar>')->getSuffix());
+    }
+
     public function testSuffixStringable(): void
     {
         $instance = new class () {
@@ -66,5 +126,29 @@ final class HasPrefixAndSuffixTest extends TestCase
 
         $this->assertEmpty($instance->getSuffix());
         $this->assertSame('bar', $instance->suffix($suffix)->getSuffix());
+    }
+
+    public function testSuffixStringableSpecialChar(): void
+    {
+        $instance = new class () {
+            use HasPrefixAndSuffix;
+
+            protected array $attributes = [];
+
+            public function getSuffix(): string
+            {
+                return $this->suffix;
+            }
+        };
+
+        $suffix = new class () implements Stringable {
+            public function __toString(): string
+            {
+                return 'bar && baz';
+            }
+        };
+
+        $this->assertEmpty($instance->getSuffix());
+        $this->assertSame('bar && baz', $instance->suffix($suffix)->getSuffix());
     }
 }
