22ea4faaadfe60acda7046bbabe43ec0c0441486
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-07-24 15:17:26 -0400

    Add `loadXML()` in `Encode::class`. (#58)

--- tests/Helper/EncodeTest.php
+++ tests/Helper/EncodeTest.php
@@ -25,15 +25,16 @@ final class EncodeTest extends TestCase
     public function testIsInvalidTag(): void
     {
         $this->assertFalse(Encode::isValidTag(''));
+        $this->assertFalse(Encode::isValidTag('<div'));
     }
 
     public function testIsValidSvg(): void
     {
-        $this->assertFalse(
+        $this->assertTrue(
             Encode::isValidTag(
                 <<<HTML
                 <svg class='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
-                    <path fill-rule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clip-rule='evenodd'></path>
+                <path fill-rule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clip-rule='evenodd'></path>
                 </svg>
                 HTML
             )
