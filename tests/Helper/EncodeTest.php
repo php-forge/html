4eca0e7cec39e3bf1b87988133b239423aca1e48
Wilmer Arambula <terabytesoftw@gmail.com>
2023-07-24 13:54:49 -0400

    Fix `svg` validation.

--- tests/Helper/EncodeTest.php
+++ tests/Helper/EncodeTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace PHPForge\Html\Tests\Helper;
 
 use PHPForge\Html\Helper\Encode;
+use PHPForge\Html\Svg;
 use PHPUnit\Framework\TestCase;
 
 final class EncodeTest extends TestCase
@@ -22,9 +23,27 @@ final class EncodeTest extends TestCase
         $this->assertSame($expected, Encode::content($value, $doubleEncode));
     }
 
-    public function testIsInvalidTagContent(): void
+    public function testIsInvalidTag(): void
     {
         $this->assertFalse(Encode::isValidTag(''));
+    }
+
+    public function testIsValidSvg(): void
+    {
+        $this->assertFalse(
+            Encode::isValidTag(
+                <<<HTML
+                <svg class='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+                    <path fill-rule='evenodd' d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z' clip-rule='evenodd'></path>
+                </svg>
+                HTML
+            )
+        );
+    }
+
+    public function testIsInvalidSvg(): void
+    {
+        $this->assertFalse(Encode::isValidTag('<svg'));
     }
 
     /**
