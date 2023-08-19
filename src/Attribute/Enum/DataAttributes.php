550693d779ae58a5dbcb4dc0bcec3fc1092814a9
Wilmer Arambula <terabytesoftw@gmail.com>
2023-08-19 07:36:11 -0400

    Add more attributes.

--- src/Attribute/Enum/DataAttributes.php
+++ src/Attribute/Enum/DataAttributes.php
@@ -9,6 +9,8 @@ enum DataAttributes: string
     case ACTION = 'data-action';
     case CANCEL_TEXT = 'data-cancel-text';
     case CONFIRM_TEXT = 'data-confirm-text';
+    case DATA_DRAWER_TARGET = 'data-drawer-target';
+    case DATA_DRAWER_TOGGLE = 'data-drawer-toggle';
     case ICON = 'data-icon';
     case METHOD = 'data-method';
     case MESSAGE = 'data-message';
