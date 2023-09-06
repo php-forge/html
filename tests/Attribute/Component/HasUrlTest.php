e47157801b4d5373d155b584b88dcb78f5017c6e
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-09-06 10:53:38 -0300

    Add `HasUrl::class`. (#118)

---
+++ tests/Attribute/Component/HasUrlTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPForge\Html\Tests\Attribute\Component;
+
+use PHPForge\Html\Attribute\Component\HasUrl;
+use PHPUnit\Framework\TestCase;
+
+final class HasUrlTest extends TestCase
+{
+    public function testImmutablity(): void
+    {
+        $instance = new class() {
+            use HasUrl;
+        };
+
+        $this->assertNotSame($instance, $instance->urlCreator(fn () => ''));
+        $this->assertNotSame($instance, $instance->urlQueryParameters([]));
+        $this->assertNotSame($instance, $instance->urlPath(''));
+    }
+}
