6f4a2140dde91b92445816a823802225724e15a0
Wilmer Arambula <terabytesoftw@gmail.com>
2023-09-06 10:49:32 -0300

    Add `HasUrl::class`.

---
+++ src/Attribute/Component/HasUrl.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPForge\Html\Attribute\Component;
+
+use Closure;
+
+/**
+ * Is used by widgets which implement the url methods.
+ */
+trait HasUrl
+{
+    protected Closure $urlCreator;
+    protected array $urlQueryParameters = [];
+    protected string $urlPath = '';
+
+    /**
+     * Create the url for the route.
+     *
+     * @param Closure $value The url creator of the route.
+     *
+     * @return static A new instance of the current class with the specified url creator.
+     */
+    public function urlCreator(Closure $value): self
+    {
+        $new = clone $this;
+        $new->urlCreator = $value;
+
+        return $new;
+    }
+
+    /**
+     * Set the url query parameters for the route.
+     *
+     * @param array $value The query parameters of the route.
+     *
+     * @return static A new instance of the current class with the specified url query parameters.
+     */
+    public function urlQueryParameters(array $value): static
+    {
+        $new = clone $this;
+        $new->urlQueryParameters = $value;
+
+        return $new;
+    }
+
+    /**
+     * Set the url path for the route.
+     *
+     * @param string $value The url path of the route.
+     *
+     * @return static A new instance of the current class with the specified url path.
+     */
+    public function urlPath(string $value): static
+    {
+        $new = clone $this;
+        $new->urlPath = $value;
+
+        return $new;
+    }
+}
