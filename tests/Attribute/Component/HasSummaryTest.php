b6f2fd16a464e9446aac6c3f731cbfd499cd634c
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-09-06 13:17:48 -0300

    Add `HasSummary::class`. (#120)

---
+++ tests/Attribute/Component/HasSummaryTest.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPForge\Html\Tests\Attribute\Component;
+
+use PHPForge\Html\Attribute\Component\HasSummary;
+use PHPUnit\Framework\TestCase;
+
+final class HasSummaryTest extends TestCase
+{
+    public function testClass(): void
+    {
+        $instance = new class() {
+            use HasSummary;
+
+            public function getSummaryClass(): string
+            {
+                return $this->summaryAttributes['class'] ?? '';
+            }
+        };
+
+        $this->assertEmpty($instance->getSummaryClass());
+
+        $instance = $instance->summaryClass('foo');
+
+        $this->assertSame('foo', $instance->getSummaryClass());
+
+        $instance = $instance->summaryClass('bar');
+
+        $this->assertSame('foo bar', $instance->getSummaryClass());
+    }
+
+    public function testImmutablity(): void
+    {
+        $instance = new class() {
+            use HasSummary;
+        };
+
+        $this->assertNotSame($instance, $instance->summaryAttributes([]));
+        $this->assertNotSame($instance, $instance->summaryClass(''));
+        $this->assertNotSame($instance, $instance->summaryLabel(''));
+        $this->assertNotSame($instance, $instance->summarySeparator(''));
+    }
+}
