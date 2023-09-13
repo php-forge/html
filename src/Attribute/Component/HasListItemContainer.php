8f0aefab52475a534ff1a611d165c01cbd26ba07
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-09-13 10:34:46 -0300

    Add `HasListItemContainer::class`. (#131)

---
+++ src/Attribute/Component/HasListItemContainer.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPForge\Html\Attribute\Component;
+
+use InvalidArgumentException;
+use PHPForge\Html\Helper\CssClass;
+
+/**
+ * Is used by widgets that implement container methods.
+ */
+trait HasListItemContainer
+{
+    protected bool $listItemContainer = false;
+    protected array $listItemContainerAttributes = [];
+
+    /**
+     * Enable or disable the container tag for list items.
+     *
+     * @param bool $value `true` to enable the container, `false` to disable.
+     *
+     * @return static A new instance of the current class with the specified container for list items.
+     */
+    public function listItemcontainer(bool $value): static
+    {
+        $new = clone $this;
+        $new->listItemContainer = $value;
+
+        return $new;
+    }
+
+    /**
+     * Set the `HTML` attributes for the container for list items.
+     *
+     * @param array $values Attribute values indexed by attribute names.
+     *
+     * @return static A new instance of the current class with the specified container attributes for list items.
+     */
+    public function listItemContainerAttributes(array $values = []): static
+    {
+        $new = clone $this;
+        $new->listItemcontainerAttributes = $values;
+
+        return $new;
+    }
+
+    /**
+     * Set the `CSS` class for the container for list items.
+     *
+     * @param string $value The CSS class name.
+     *
+     * @return static A new instance of the current class with the specified container class for list items.
+     */
+    public function listItemContainerClass(string $value): static
+    {
+        $new = clone $this;
+        CssClass::add($new->listItemContainerAttributes, $value);
+
+        return $new;
+    }
+
+    /**
+     * Set the container tag name for list items.
+     *
+     * @param string $value The tag name for the container element for list items.
+     *
+     * @return static A new instance of the current class with the specified container tag for list items.
+     *
+     * @throws InvalidArgumentException If the container tag is an empty string.
+     */
+    public function listItemContainerTag(string $value): static
+    {
+        if ($value === '') {
+            throw new InvalidArgumentException('The container tag must be a non-empty string.');
+        }
+
+        $new = clone $this;
+        $new->listItemcontainerTag = $value;
+
+        return $new;
+    }
+}
