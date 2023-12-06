d2a6dea9770ce99bed7dbf03cffe2672dd39cc1b
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2023-12-06 14:20:35 -0300

    Remove unused code related to menu attributes and menu class. (#149)

--- tests/Attribute/Component/HasMenuTest.php
+++ tests/Attribute/Component/HasMenuTest.php
@@ -9,28 +9,6 @@ use PHPUnit\Framework\TestCase;
 
 final class HasMenuTest extends TestCase
 {
-    public function testClass(): void
-    {
-        $instance = new class () {
-            use HasMenu;
-
-            public function getMenuClass(): string
-            {
-                return $this->menuAttributes['class'] ?? '';
-            }
-        };
-
-        $this->assertEmpty($instance->getMenuClass());
-
-        $instance = $instance->menuClass('foo');
-
-        $this->assertSame('foo', $instance->getMenuClass());
-
-        $instance = $instance->menuClass('bar');
-
-        $this->assertSame('foo bar', $instance->getMenuClass());
-    }
-
     public function testImmutablity(): void
     {
         $instance = new class () {
@@ -38,7 +16,5 @@ final class HasMenuTest extends TestCase
         };
 
         $this->assertNotSame($instance, $instance->menu(''));
-        $this->assertNotSame($instance, $instance->menuAttributes([]));
-        $this->assertNotSame($instance, $instance->menuClass(''));
     }
 }
