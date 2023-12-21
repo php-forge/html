56a26dc8737661d177a5de88c67fd086368fa39c
Wilmer Arambula <terabytesoftw@gmail.com>
2023-12-21 06:01:32 -0300

    Remove unused method getRuleHtmlByAttribute() in RuleHtmlByAttribute interface.

--- src/Input/RuleHtmlByAttribute.php
+++ src/Input/RuleHtmlByAttribute.php
@@ -6,10 +6,4 @@ namespace PHPForge\Html\Input;
 
 interface RuleHtmlByAttribute
 {
-    /**
-     * @param string $attribute The attribute name.
-     *
-     * @return array The rule validation for the specified attribute.
-     */
-    public function getRuleHtmlByAttribute(string $attribute): array;
 }
