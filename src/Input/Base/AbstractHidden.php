c0f579bef4a597a59b1602d81883083c51f8722c
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-11 07:09:15 -0300

    Refactor imports in AbstractHidden and AbstractInput.

--- src/Input/Base/AbstractHidden.php
+++ src/Input/Base/AbstractHidden.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace PHPForge\Html\Input\Base;
 
-use PHPForge\Html\{Attribute, Tag};
 use PHPForge\Html\Input\Contract\ValueInterface;
+use PHPForge\Html\{Attribute, Tag};
 use PHPForge\Widget\Element;
 
 abstract class AbstractHidden extends Element implements ValueInterface
