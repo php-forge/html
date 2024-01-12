90ad8bbb5aa97ddb9a2974d9468e7f901e59fd86
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-12 18:12:20 -0300

    Refactor HasIdTest class.

--- tests/Attribute/HasIdTest.php
+++ tests/Attribute/HasIdTest.php
@@ -19,33 +19,4 @@ final class HasIdTest extends TestCase
 
         $this->assertNotSame($instance, $instance->id(''));
     }
-
-    public function testGenerateId(): void
-    {
-        $instance = new class () {
-            use HasId;
-
-            protected array $attributes = [];
-        };
-
-        $instance = $instance->id('');
-
-        $this->assertNotEmpty($instance->generateId());
-        $this->assertMatchesRegularExpression('/^id_[a-z0-9]{13}$/', $instance->generateId());
-        $this->assertMatchesRegularExpression('/^alert_[a-z0-9]{13}$/', $instance->id('')->generateId('alert_'));
-    }
-
-    public function testGenerateIdWithId(): void
-    {
-        $instance = new class () {
-            use HasId;
-
-            protected array $attributes = [];
-        };
-
-
-        $instance = $instance->id('foo');
-
-        $this->assertSame('foo', $instance->generateId());
-    }
 }
