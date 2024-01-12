b929123bde7c3ca287ac42d0dd73db6372279d83
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-12 11:55:06 -0300

    Add data-bs-dismiss attribute to ButtonToggle class and corresponding tests.

--- tests/ButtonToggle/RenderTest.php
+++ tests/ButtonToggle/RenderTest.php
@@ -63,6 +63,156 @@ final class RenderTest extends TestCase
         );
     }
 
+    public function testDataBsAutoClose(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-bs-auto-close="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataBsAutoClose('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataBsDismiss(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-bs-dismiss="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataBsDismiss('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataBsTarget(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-bs-target="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataBsTarget('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataBsTargetWithTrue(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-bs-target="#toggle-id"></button>
+            HTML,
+            ButtonToggle::widget()
+                ->dataBsTarget(true)
+                ->id('button-toggle-658716145f1d9')
+                ->toggleId('toggle-id')
+                ->render()
+        );
+    }
+
+    public function testDataBsToggle(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-bs-toggle="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataBsToggle('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataDismissTarget(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-dismiss-target="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataDismissTarget('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataDismissTargetWithTrue(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-dismiss-target="toggle-id"></button>
+            HTML,
+            ButtonToggle::widget()
+                ->dataDismissTarget(true)
+                ->id('button-toggle-658716145f1d9')
+                ->toggleId('toggle-id')
+                ->render()
+        );
+    }
+
+    public function testDataDrawerTarget(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-drawer-target="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataDrawerTarget('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataDrawerTargetWithTrue(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-drawer-target="toggle-id"></button>
+            HTML,
+            ButtonToggle::widget()
+                ->dataDrawerTarget(true)
+                ->id('button-toggle-658716145f1d9')
+                ->toggleId('toggle-id')
+                ->render()
+        );
+    }
+
+    public function testDataDropdownToggle(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-dropdown-toggle="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataDropdownToggle('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataDropdownToggleWithTrue(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-dropdown-toggle="toggle-id"></button>
+            HTML,
+            ButtonToggle::widget()
+                ->dataDropdownToggle(true)
+                ->id('button-toggle-658716145f1d9')
+                ->toggleId('toggle-id')
+                ->render()
+        );
+    }
+
+    public function testDataToggle(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-toggle="value"></button>
+            HTML,
+            ButtonToggle::widget()->dataToggle('value')->id('button-toggle-658716145f1d9')->render()
+        );
+    }
+
+    public function testDataToggleWithTrue(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <button id="button-toggle-658716145f1d9" type="button" data-toggle="toggle-id"></button>
+            HTML,
+            ButtonToggle::widget()
+                ->dataToggle(true)
+                ->id('button-toggle-658716145f1d9')
+                ->toggleId('toggle-id')
+                ->render()
+        );
+    }
+
     public function testName(): void
     {
         Assert::equalsWithoutLE(
