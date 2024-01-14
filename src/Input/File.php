1740ba12e1b94d072841ffbc88b2ff03edac993e
Wilmer Arambula <terabytesoftw@gmail.com>
2024-01-14 08:50:29 -0300

    Add RequiredInterface to File input element.

--- src/Input/File.php
+++ src/Input/File.php
@@ -6,6 +6,7 @@ namespace PHPForge\Html\Input;
 
 use PHPForge\Html\Attribute;
 use PHPForge\Html\Helper\Utils;
+use PHPForge\Html\Input\Contract\RequiredInterface;
 
 /**
  * The input element with a type attribute whose value is "file" represents a list of file items, each consisting of a
@@ -13,10 +14,11 @@ use PHPForge\Html\Helper\Utils;
  *
  * @link https://www.w3.org/TR/2012/WD-html-markup-20120329/input.file.html#input.file
  */
-final class File extends Base\AbstractInput
+final class File extends Base\AbstractInput implements RequiredInterface
 {
     use Attribute\Custom\HasUnchecked;
     use Attribute\Input\CanBeMultiple;
+    use Attribute\Input\CanBeRequired;
     use Attribute\Input\HasAccept;
 
     protected function loadDefaultDefinitions(): array
