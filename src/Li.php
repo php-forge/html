95d296544faf5ab6e1f15b35d9bde61254e4f52d
terabytesoftw <terabytesoftw@users.noreply.github.com>
2024-01-24 18:05:05 +0000

    Apply fixes from Easy Coding Standard.

--- src/Li.php
+++ src/Li.php
@@ -12,5 +12,4 @@ namespace PHPForge\Html;
  *
  * @link https://html.spec.whatwg.org/multipage/grouping-content.html#the-li-element
  */
-final class Li extends Base\AbstractLi {
-}
+final class Li extends Base\AbstractLi {}
