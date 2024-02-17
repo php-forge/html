e5e5c7fab6d27e83fd9b10cf09ec727cb59f5d74
Wilmer Arambula <terabytesoftw@gmail.com>
2024-02-17 13:41:38 -0300

    add doc input `File::class`.

---
+++ tests/Input/File/ValidateTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPForge\Html\Tests\Input\File;
+
+use PHPForge\Html\Input\File;
+use PHPForge\Support\Assert;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+final class ValidateTest extends TestCase
+{
+    public function testAccept(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <input id="file-65a15e0439570" type="file" accept="value">
+            HTML,
+            File::widget()->accept('value')->id('file-65a15e0439570')->render()
+        );
+    }
+
+    public function testRequired(): void
+    {
+        Assert::equalsWithoutLE(
+            <<<HTML
+            <input id="file-65a15e0439570" type="file" required>
+            HTML,
+            File::widget()->id('file-65a15e0439570')->required()->render()
+        );
+    }
+}
